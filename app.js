8b125da40baed2839c2710ac6ff806a0099df2d2
knyttali <amanda_andrae@hotmail.com>
2022-02-17 11:53:35 +0100

    kk

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use("/healthcheck", require("./routes/healthcheck.routes"));
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +13,31 @@ app.get("/", (req, res) => {
   body = { status: "available" };
   res.status(200).send(body);
 });
+
+app.get("/football", (req ,res)=>{
+  headers={"http_status":200, "cache-control": "no-cache"}
+  body=
+  [
+      {
+          "name": "AIK",
+          "points": 9
+      },
+      {
+          "name": "DIF",
+          "points": 6
+      },
+      {
+          "name": "BP",
+          "points": 5
+      },
+      {
+          "name": "HSK",
+          "points": 3
+      }
+  ]
+  res.set('Content-Type', 'application/json')
+  res.status(200).send(body)
+})
 
 app.listen(PORT, () => {
   console.log(`STARTED LISTENING ON PORT ${PORT}`);
