fb51ea83d6004c2420f5d5787bf74659b436bafa
knyttali <amanda_andrae@hotmail.com>
2022-02-24 10:34:25 +0100

    blaha till hej

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-const cors = require("cors")
-const express = require("express")
+const cors = require("cors");
+const express = require("express");
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use('/healthcheck', require('./routes/healthcheck.routes'));
-app.use(express.urlencoded( { extended: true }));
-app.use(cors())
+app.use("/healthcheck", require("./routes/healthcheck.routes"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get("/", (req ,res)=>{
-    headers={"http_status":200, "cache-control": "no-cache"}
-    body={"blaha":"blä"}
-    res.set('Content-Type', 'application/json')
-    res.status(200).send(body)
+app.get("/", (req, res) => {
+  headers = { http_status: 200, "cache-control": "no-cache" };
+  body = { hej: "hå" };
+  res.set("Content-Type", "application/json");
+  res.status(200).send(body);
 });
 
-app.get("/football", (req ,res)=>{
-    headers={"http_status":200, "cache-control": "no-cache"}
-    body=
-    [
-        {
-            "name": "AIK",
-            "points": 9
-        },
-        {
-            "name": "DIF",
-            "points": 6
-        },
-        {
-            "name": "BP",
-            "points": 5
-        },
-        {
-            "name": "HSK",
-            "points": 3
-        }
-    ]
-    res.set('Content-Type', 'application/json')
-    res.status(200).send(body)
-})
+app.get("/football", (req, res) => {
+  headers = { http_status: 200, "cache-control": "no-cache" };
+  body = [
+    {
+      name: "AIK",
+      points: 9,
+    },
+    {
+      name: "DIF",
+      points: 6,
+    },
+    {
+      name: "BP",
+      points: 5,
+    },
+    {
+      name: "HSK",
+      points: 3,
+    },
+  ];
+  res.set("Content-Type", "application/json");
+  res.status(200).send(body);
+});
 
-app.listen(PORT , ()=>{
-    console.log(`STARTED LISTENING ON PORT ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`STARTED LISTENING ON PORT ${PORT}`);
 });
